c3d702cd846c2329b9260a84b142bb1d013593a2
ihor000 <ihor.silnov@yandex.ru>
2022-12-11 18:43:05 +0300

    text

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int degreeOfNumber = numberA;
+
+for(int i = 1; numberB >= i; i++)
+{
+    degreeOfNumber = degreeOfNumber * numberA;
+    Console.WriteLine(degreeOfNumber);
+   
+   
+}
