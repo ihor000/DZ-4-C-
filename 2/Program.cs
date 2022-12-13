ecb450cc293604ba89d083d589b234110dddc5dd
ihor000 <ihor.silnov@yandex.ru>
2022-12-14 02:11:40 +0300

    text

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,7 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int n = Convert.ToInt32(Console.ReadLine());
+     int sum = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            if (n % i == 0) sum += i;
+        }
+        Console.WriteLine(sum);
